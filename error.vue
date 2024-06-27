02be08944fccffed703293d8e8118d81a5b71a53
Niko Laasanen <niko.laasanen@gmail.com>
2024-06-27 21:50:04 +0300

    Pretty error page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-if="error" class="h-lvh grid place-content-center">
+        <div class="flex items-center gap-6">
+            <img class="aspect-square" src="https://xivapi.com/i/002000/002914_hr1.png" alt="Rabbit Medium icon" />
+            <h2 class="text-8xl">{{ error.statusCode }}</h2>
+        </div>
+        <p class="text-center">{{ error.message }}</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import type { NuxtError } from '#app'
+
+defineProps({
+    error: Object as () => NuxtError
+})
+</script>
